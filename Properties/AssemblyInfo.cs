1a770f183998dcf04153047d5fc36f688d5fb685
rendeer-pl <wojtek@rendeer.pl>
2021-09-09 01:02:24 +0200

    vid fixed app versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
