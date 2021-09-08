fbf47a52f9ea8af819dd0a500037f139d74c9aac
rendeer-pl <wojtek@rendeer.pl>
2021-09-09 01:12:21 +0200

    vid VID-44 trimming logic extracted to a separate class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Forms;
 using Path = System.IO.Path;
+using static VideoTrimmer.VideoProcessing;
 
 namespace VideoTrimmer
 {
@@ -15,8 +16,7 @@ namespace VideoTrimmer
     public partial class MainWindow : Window
     {
 
-        string File = null;
-        TimeSpan FileDuration;
+        VideoProcessing videoProcessing = new VideoProcessing();
         
         public MainWindow()
         {
@@ -24,7 +24,7 @@ namespace VideoTrimmer
 
             // Updating the "About" footer
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
-            aboutFooter.Content = "Rendeer " + version.Major + "." + version.Minor + "." + version.Build + ".190521";
+            aboutFooter.Content = "Rendeer " + version.Major + "." + version.Minor + "." + version.Build + ".190525";
         }
 
         // Used to enable or disable editable fields
@@ -64,38 +64,41 @@ namespace VideoTrimmer
             fileNameLabel.ToolTip = "No video selected";
             timecodeStart.Text = "00:00:00";
             timecodeEnd.Text = "00:00:00";
-            File = null;
+            videoProcessing = null;
             SetFieldsLockStatus(false);
 
             return;
         }
 
         // Analyzing a file selected via drag and dopping or using "Open File" dialog
-        private void OpenSelectedFile(string FileToBeOpened)
+        private void OpenSelectedFile(string FileToOpen)
         {
-            Console.WriteLine("Opening file: " + FileToBeOpened);
-            File = FileToBeOpened;
-            String FileName = Path.GetFileName(File);
-            String FileRoot = Path.GetPathRoot(File);
+            Console.WriteLine("Opening file: " + FileToOpen);
+
+            if (videoProcessing.LoadFile(FileToOpen)==true)
+            {
+                Console.WriteLine("File opened successfully");
+            } else
+            {
+                Console.WriteLine("Couldn't open file");
+            }
+            
 
             // Unlock editable fields
             SetFieldsLockStatus(true);
 
             // Get video duration and paste it into "End" timecode TextBox
-            var player = new WindowsMediaPlayer();
-            var clip = player.newMedia(File);
-            FileDuration = TimeSpan.FromSeconds(clip.duration);
             timecodeStart.Text = "00:00:00";
-            timecodeEnd.Text = FileDuration.ToString();
+            timecodeEnd.Text = videoProcessing.GetDuration().ToString();
 
             // If file path is long, trim it
             String FileNameToDisplay = "";
-            if (File.Length > 40) FileNameToDisplay = FileRoot + "...\\" + FileName;
-            else FileNameToDisplay = File;
+            if (videoProcessing.GetFilePath().Length > 40) FileNameToDisplay = videoProcessing.GetFileRoot() + "...\\" + videoProcessing.GetFileName();
+            else FileNameToDisplay = videoProcessing.GetFilePath();
 
             // Update display
             fileNameLabel.Content = "✔️ " + FileNameToDisplay;
-            fileNameLabel.ToolTip = File;
+            fileNameLabel.ToolTip = videoProcessing.GetFilePath();
 
             return;
         }
@@ -113,8 +116,7 @@ namespace VideoTrimmer
             switch (result)
             {
                 case System.Windows.Forms.DialogResult.OK:
-                    File = fileDialog.FileName;
-                    OpenSelectedFile(File);
+                    OpenSelectedFile(fileDialog.FileName);
                     break;
                 case System.Windows.Forms.DialogResult.Cancel:
                 default:
@@ -131,8 +133,8 @@ namespace VideoTrimmer
             // check if a value is a valid TimeSpan
             if (TimeSpan.TryParse(senderTextBox.Text, out _)) 
             {
-                // check if the value is shorter or equal to FileDUration
-                if (TimeSpan.Parse(senderTextBox.Text) <= FileDuration) 
+                // check if the value is shorter or equal to FileDuration
+                if (TimeSpan.Parse(senderTextBox.Text) <= videoProcessing.GetDuration()) 
                 {
                     // make sure that none of the TextBoxes is null
                     if (timecodeStart == null | timecodeEnd == null) 
@@ -160,18 +162,11 @@ namespace VideoTrimmer
         }
 
 
-        // Handles output from the transcoding process
-        public void ProcessEventHandler(object e, DataReceivedEventArgs outLine)
-        {
-            System.Console.WriteLine(DateTime.Now + " - " + outLine.Data);
-        }
-
-
         // main logic - "TRIM VIDEO" button click
         private void ButtonTrimVideo_Click(object sender, RoutedEventArgs e)
         {
             // Make sure the file exists
-            if (!System.IO.File.Exists(File))
+            if (!videoProcessing.DoesTheFileExist())
             {
                 // TODO: Replace with a custom error window
                 System.Windows.MessageBox.Show("Source video has been moved or deleted!");
@@ -183,100 +178,31 @@ namespace VideoTrimmer
             }
 
             // establish Start and Duration for the needs of the console command
-            string Start = timecodeStart.Text;
-            TimeSpan Duration = TimeSpan.Parse(timecodeEnd.Text) - TimeSpan.Parse(timecodeStart.Text);
-
-            // CONSOLE COMMAND START
-            String FilePath = System.IO.Path.GetDirectoryName(File);
-            String FileName = System.IO.Path.GetFileNameWithoutExtension(File);
-            String FileExtension = System.IO.Path.GetExtension(File);
-
-            // find a unique filename
-            String NewPartialPath = FilePath + "\\" + FileName + "_trim_";
-            int UniqueFilenameIndex = 1;
-            bool FoundUniqueName = false;
-            while (FoundUniqueName == false)
-            {
-                if (System.IO.File.Exists(NewPartialPath + UniqueFilenameIndex + FileExtension))
-                {
-                    Console.WriteLine("Name already taken: " + NewPartialPath + UniqueFilenameIndex + FileExtension);
-                    UniqueFilenameIndex++;
-                }
-                else
-                {
-                    Console.WriteLine("Name available: " + NewPartialPath + UniqueFilenameIndex + FileExtension);
-                    FoundUniqueName = true;
-                }
-            }
-            String NewFileName = NewPartialPath + UniqueFilenameIndex + FileExtension;
-
-
-            // forge the command
-            String ConsoleCommand = "/C ffmpeg -ss " + Start + " -i \"" + File + "\" -to " + Duration + " ";
-
-            // are we recompressing the video?
-            if (recompressFile.IsChecked == false) ConsoleCommand += "-c copy ";
-
-            // are we removing audio?
-            if (removeAudio.IsChecked == true) ConsoleCommand += "-an ";
-
-            ConsoleCommand += "\"" + NewFileName + "\"";
-            // CONSOLE COMMAND END
-
-
-            // Prepare the process to execute the command
-            Process process = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo
-            {
-                WindowStyle = ProcessWindowStyle.Normal,
-                FileName = "cmd.exe",
-                Arguments = ConsoleCommand,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                CreateNoWindow = true
-            };
-
-            // bind methods to receiving output data from pricess
-            process.OutputDataReceived += new DataReceivedEventHandler(ProcessEventHandler);
-            process.ErrorDataReceived += new DataReceivedEventHandler(ProcessEventHandler);
-
-            // Launch the process and start listening for output
-            process.StartInfo = startInfo;
-            process.Start();
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.WaitForExit();
-
+            TimeSpan Start = TimeSpan.Parse(timecodeStart.Text);
+            TimeSpan End = TimeSpan.Parse(timecodeEnd.Text);
 
             // initialize vars that will be used in results window
             string caption;
             string message;
 
-            // Command executed successfully
-            if (process.ExitCode == 0) 
-            {
-                caption = "Everything should be ok";
-                message = "Video trimmed using the following command:\n\r\n\r" + ConsoleCommand.Substring(3);
+            ResultsWindowContents result = videoProcessing.Execute(Start, End, (bool)removeAudio.IsChecked, (bool)recompressFile.IsChecked);
 
+            if (result.success==true)
+            {
                 // Displays the MessageBox
                 // TODO: Replace with a custom window
-                _ = System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+                _ = System.Windows.Forms.MessageBox.Show(result.message, result.caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 // Open the folder
-                string argument = "/select, \"" + NewFileName + "\"";
+                string argument = "/select, \"" + result.newFileName + "\"";
                 Process.Start("explorer.exe", argument);
-            }
-            // Command failed
-            else
+            } else
             {
-                caption = "Something went wrong";
-                message = "Command that was used:\n\r\n\r" + ConsoleCommand.Substring(3);
-
                 // Displays the MessageBox
                 // TODO: Replace with a custom window
-                _ = System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+                _ = System.Windows.Forms.MessageBox.Show(result.message, result.caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
         }
 
         // Displays "About" window
@@ -303,16 +229,15 @@ namespace VideoTrimmer
                 string draggedFile = files[0];
 
                 // check if it has a compatible extension
-                // TODO: compare against an enum of supported extensions
-                if (Path.GetExtension(draggedFile) == ".mp4")
+                if (videoProcessing.CheckIfFileIsAccepted(draggedFile))
                 {
                     Console.WriteLine("Dragged file recognized!");
-                    Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0xFF, 0, 0x77, 0));
+                    Background = new SolidColorBrush(Color.FromArgb(0xFF, 0, 0x77, 0));
                 }
                 else
                 {
                     Console.WriteLine("Dragged file not recognized.");
-                    Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0xFF, 0x88, 0, 0));
+                    Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x88, 0, 0));
                 }
             }
         }
@@ -324,15 +249,14 @@ namespace VideoTrimmer
             string[] files = (string[])(e.Data.GetData("FileDrop", false));
             
             // restore default background
-            Background = System.Windows.Media.Brushes.Black;
+            Background = Brushes.Black;
                         
             // get the first file, ignore the rest
             string droppedFile = files[0];
             Console.WriteLine(Path.GetExtension(droppedFile));
             
             // check if it has a compatible extension
-            // TODO: compare against an enum of supported extensions
-            if (Path.GetExtension(droppedFile)==".mp4")
+            if (videoProcessing.CheckIfFileIsAccepted(droppedFile))
             {
                 Console.WriteLine("Dropped file recognized!");
                 OpenSelectedFile(droppedFile);
@@ -346,7 +270,7 @@ namespace VideoTrimmer
         // Fired whenever an object that has been dragged over the app window has left the window
         private void Window_DragLeave(object sender, System.Windows.DragEventArgs e)
         {
-            Background = System.Windows.Media.Brushes.Black;
+            Background = Brushes.Black;
         }
     }
 }
---
+++ VideoProcessing.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Path = System.IO.Path;
+using WMPLib;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace VideoTrimmer
+{
+    public class VideoProcessing
+    {
+        String FilePath;
+        String FileName;
+        String FileRoot;
+        String FileExtension;
+        String FileDirectory;
+        TimeSpan FileDuration;
+        WindowsMediaPlayer Player = new WindowsMediaPlayer();
+        ResultsWindowContents result;
+
+        public bool LoadFile(string File)
+        {
+            FilePath = File;
+            FileName = Path.GetFileName(FilePath);
+            FileRoot = Path.GetPathRoot(FilePath);
+            FileExtension = Path.GetExtension(FilePath);
+            FileDirectory = Path.GetDirectoryName(FilePath);
+
+            // Get video duration
+            var clip = Player.newMedia(FilePath);
+            FileDuration = TimeSpan.FromSeconds(clip.duration);
+
+            return false;
+        }
+
+        public string GetFilePath()
+        {
+            return FilePath;
+        }
+
+        public string GetFileName() {
+            return FileName;
+        }
+
+        public string GetFileRoot()
+        {
+            return FileRoot;
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return FileDuration;
+        }
+
+        public bool CheckIfFileIsAccepted(string FileToCheck)
+        {
+            // TODO: compare against an enum of supported extensions
+            if (Path.GetExtension(FileToCheck) == ".mp4")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DoesTheFileExist() => System.IO.File.Exists(FilePath);
+
+        // Handles output from the transcoding process
+        private void ProcessEventHandler(object e, DataReceivedEventArgs outLine)
+        {
+            System.Console.WriteLine(DateTime.Now + " - " + outLine.Data);
+        }
+
+
+        public struct ResultsWindowContents
+        {
+            public bool success;
+            public String caption;
+            public String message;
+            public String newFileName;
+            public ResultsWindowContents(bool p1, String p2, String p3, String p4)
+            {
+                success = p1;
+                caption = p2;
+                message = p3;
+                newFileName = p4;
+            }
+        }
+
+        public ResultsWindowContents Execute(TimeSpan Start, TimeSpan End, bool ShouldRemoveAudio, bool ShouldRecompress)
+        {
+
+            TimeSpan Duration = End - Start;
+
+            // find a unique filename
+            String NewPartialPath = FileDirectory + "\\" + FileName + "_trim_";
+            int UniqueFilenameIndex = 1;
+            bool FoundUniqueName = false;
+            while (FoundUniqueName == false)
+            {
+                if (System.IO.File.Exists(NewPartialPath + UniqueFilenameIndex + FileExtension))
+                {
+                    Console.WriteLine("Name already taken: " + NewPartialPath + UniqueFilenameIndex + FileExtension);
+                    UniqueFilenameIndex++;
+                }
+                else
+                {
+                    Console.WriteLine("Name available: " + NewPartialPath + UniqueFilenameIndex + FileExtension);
+                    FoundUniqueName = true;
+                }
+            }
+            String NewFileName = NewPartialPath + UniqueFilenameIndex + FileExtension;
+
+
+            // forge the command
+            String ConsoleCommand = "/C ffmpeg -ss " + Start.ToString() + " -i \"" + FilePath + "\" -t " + Duration.ToString() + " ";
+
+            // are we recompressing the video?
+            if (ShouldRecompress == false) ConsoleCommand += "-c copy ";
+
+            // are we removing audio?
+            if (ShouldRemoveAudio == true) ConsoleCommand += "-an ";
+
+            ConsoleCommand += "\"" + NewFileName + "\"";
+            // CONSOLE COMMAND END
+
+
+            // Prepare the process to execute the command
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                Arguments = ConsoleCommand,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            // bind methods to receiving output data from pricess
+            process.OutputDataReceived += new DataReceivedEventHandler(ProcessEventHandler);
+            process.ErrorDataReceived += new DataReceivedEventHandler(ProcessEventHandler);
+
+            // Launch the process and start listening for output
+            process.StartInfo = startInfo;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            // Command executed successfully
+            if (process.ExitCode == 0)
+            {
+                result.success = true;
+                result.caption = "Everything should be ok";
+                result.message = "Video trimmed using the following command:\n\r\n\r" + ConsoleCommand.Substring(3);
+                result.newFileName = NewFileName;
+            }
+            else
+            {
+                // Command failed
+                result.success = false;
+                result.caption = "Something went wrong";
+                result.message = "Command that was used:\n\r\n\r" + ConsoleCommand.Substring(3);
+            }
+
+            return result;
+        }
+
+    }
+}
