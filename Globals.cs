2899dbd9aaac3f2b8c14d63d81f6946a51d17c80
rendeer-pl <wojtek@rendeer.pl>
2021-09-09 01:30:10 +0200

    vid updated copyright and version

--- Globals.cs
+++ Globals.cs
@@ -6,12 +6,12 @@ public static class Globals
     public const string appTitle = "Video Trimmer";
     public const string description = "";
     public const string company = "Rendeer";
-    public const string copyright = "Copyright © Rendeer 2020";
+    public const string copyright = "Copyright © Rendeer 2021";
 
     private const string majorVersion = "0";
     private const string minorVersion = "2";
-    private const string releaseVersion = "9";
-    private const string buildDate = "200327";
+    private const string releaseVersion = "10";
+    private const string buildDate = "210908";
 
     public const string assemblyVersion = majorVersion + "." + minorVersion + "." + releaseVersion + ".0";
     public const string customVersion = majorVersion + "." + minorVersion + "." + releaseVersion + "." + buildDate;
