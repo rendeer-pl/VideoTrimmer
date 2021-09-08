ea8982178c529beb7742922988b0dc1f73f659f2
RND-1 <wojtek@rendeer.pl>
2021-09-09 00:40:56 +0200

    vid VID-5 main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,66 @@ namespace VideoTrimmer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    fileNameLabel.Content = "✅ " + file;
+                    fileNameLabel.ToolTip = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    fileNameLabel.Content = "No file selected";
+                    fileNameLabel.ToolTip = "No file selected";
+                    break;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonTrimVideo_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Validate file
+
+            // TODO: Validate timecodes
+
+            // TODO: Put together a console command
+
+            // TODO: Execute console command
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = @"/C dir /p";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            MessageBox.Show(output);
+            p.WaitForExit();
+
+
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        public void ButtonShowAbout_Click(object sender, RoutedEventArgs e)
+        {
+            string Text;
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            Text = "Rendeer " + version.Major + "." + version.Minor + ".190510";
+            MessageBox.Show(Text);
         }
     }
 }
