10d4b65d6b0748b723f38c2921ba39e455e6da9e
rendeer-pl <wojtek@rendeer.pl>
2021-10-26 02:56:45 +0200

    1.0.0 release

--- Globals.cs
+++ Globals.cs
@@ -8,10 +8,10 @@ public static class Globals
     public const string company = "Rendeer";
     public const string copyright = "Copyright © Rendeer 2021";
 
-    private const string majorVersion = "0";
-    private const string minorVersion = "2";
-    private const string releaseVersion = "10";
-    private const string buildDate = "210908";
+    private const string majorVersion = "1";
+    private const string minorVersion = "0";
+    private const string releaseVersion = "0";
+    private const string buildDate = "211026";
 
     public const string assemblyVersion = majorVersion + "." + minorVersion + "." + releaseVersion + ".0";
     public const string customVersion = majorVersion + "." + minorVersion + "." + releaseVersion + "." + buildDate;
