04d0686083994ca992f77d1a3a66ea96cc7eaba1
rendeer-pl <wojtek@rendeer.pl>
2022-06-20 01:17:10 +0200

    File name label displays extension consistently

--- VideoProcessing.cs
+++ VideoProcessing.cs
@@ -60,7 +60,7 @@ namespace VideoTrimmer
         }
 
         public string GetFileName() {
-            return FileName;
+            return FileName+FileExtension;
         }
 
         public string GetFileRoot()
